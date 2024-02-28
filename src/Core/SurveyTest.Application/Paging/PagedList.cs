e2600cd27638baa7e3891f3c12e7c38177f40033
Vladislav2123 <mangushev211104@gmail.com>
2024-02-28 18:05:01 +0300

    Add Paging

---
+++ src/Core/SurveyTest.Application/Paging/PagedList.cs
@@ -0,0 +1,29 @@
+﻿namespace SurveyTest.Application.Paging;
+
+public class PagedList<T>
+{
+    public PagedList(List<T> items, int total, Page page)
+    {
+        Items = items;
+        Total = total;
+        Page = page.number;
+        Size = page.size;
+    }
+
+    public List<T> Items { get; }
+
+    /// <summary>
+    /// Total Items amount.
+    /// </summary>
+    public int Total { get; }
+
+    /// <summary>
+    /// Current page number.
+    /// </summary>
+    public int Page { get; }
+
+    /// <summary>
+    /// Size of page.
+    /// </summary>
+    public int Size { get; }
+}
