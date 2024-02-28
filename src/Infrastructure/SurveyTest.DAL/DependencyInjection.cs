43a5d638663274add15846bec2b9c72fa09b4c79
Vladislav2123 <mangushev211104@gmail.com>
2024-02-28 17:07:19 +0300

    Add DAL Dependency Injection

---
+++ src/Infrastructure/SurveyTest.DAL/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SurveyTest.Application.Abstraction;
+
+namespace SurveyTest.DAL;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddDal(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddDbContext<ApplicationDbContext>(options => 
+            options.UseNpgsql(config.GetConnectionString("PostgreSQL")));
+
+        services.AddScoped<IApplicationDbContext>(provider =>
+            provider.GetService<ApplicationDbContext>());
+
+        return services;
+    }
+}
--- src/Presentation/SurveyTest.API/Program.cs
+++ src/Presentation/SurveyTest.API/Program.cs
@@ -1,11 +1,12 @@
+using SurveyTest.DAL;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services
+    .AddDal(builder.Configuration);
 
 var app = builder.Build();
 
