185311983feda547e1e1e53d60af683810a6efd3
Vladislav2123 <mangushev211104@gmail.com>
2024-03-04 19:24:56 +0300

    Domain entities fix

--- src/Core/SurveyTest.Domain/Entities/Interview.cs
+++ src/Core/SurveyTest.Domain/Entities/Interview.cs
@@ -5,7 +5,7 @@ public class Interview
     public Guid Id { get; set; }
 
     public User User { get; set; }
-    public User UserId { get; set; }
+    public Guid UserId { get; set; }
 
     public Survey Survey { get; set; }
     public Guid SurveyId { get; set; }
--- src/Core/SurveyTest.Domain/Entities/Survey.cs
+++ src/Core/SurveyTest.Domain/Entities/Survey.cs
@@ -6,7 +6,7 @@ public class Survey
     public string Title { get; set; }
 
     public User AuthorUser { get; set; }
-    public User AuthorUserId { get; set; }
+    public Guid AuthorUserId { get; set; }
 
     public ICollection<Question> Questions { get; set; }
     public ICollection<Interview> Interviews { get; set; }
